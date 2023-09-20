e2f2ca028f08bfa49e606d9238eb5c73ad12573c
anojdinuranga <anojdinuranga2001@gmail.com>
2023-09-20 21:33:03 +0530

    Latest Changers

--- config/onesignal.php
+++ config/onesignal.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-    'app_id' => env('ONESIGNAL_APP_ID'),
-    'rest_api_key' => env('ONESIGNAL_REST_API_KEY'),
-    'user_auth_key' => env('USER_AUTH_KEY')
+    'app_id' => env('OS_APP_ID'),
+    'rest_api_key' => env('OS_API_KEY'),
+    'user_auth_key' => env('OS_USER_AUTH_KEY')
 );
